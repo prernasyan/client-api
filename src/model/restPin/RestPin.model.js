7fe8860e102c7149d19e6107f700eef1cf86f92c
prernasyan <prernasyan22@gmail.com>
2023-04-14 23:38:56 +0530

    RestPin model created

--- src/model/restPin/RestPin.model.js
+++ src/model/restPin/RestPin.model.js
@@ -1,122 +1,58 @@
 const { token } = require("morgan");
-const { UserSchema } = require("./User.schema");
+const { ResetPinSchema } = require("./RestPin.schema");
 
-const insertUser = (userObj) => {
+const { randomPinNumber } = require("../../utils/randomGenerator");
+
+const setPasswordRestPin = async (email) => {
+  //reand 6 digit
+  const pinLength = 6;
+  const randPin = await randomPinNumber(pinLength);
+
+  const restObj = {
+    email,
+    pin: randPin,
+  };
+
   return new Promise((resolve, reject) => {
-    UserSchema(userObj)
+    ResetPinSchema(restObj)
       .save()
       .then((data) => resolve(data))
       .catch((error) => reject(error));
   });
 };
 
-const getUserByEmail = (email) => {
+const getPinByEmailPin = (email, pin) => {
   return new Promise((resolve, reject) => {
-    if (!email) return false;
-
     try {
-      UserSchema.findOne({ email }, (error, data) => {
+      ResetPinSchema.findOne({ email, pin }, (error, data) => {
         if (error) {
           console.log(error);
-          reject(error);
+          resolve(false);
         }
+
         resolve(data);
       });
     } catch (error) {
       reject(error);
-    }
-  });
-};
-
-const getUserById = (_id) => {
-  return new Promise((resolve, reject) => {
-    if (!_id) return false;
-
-    try {
-      UserSchema.findOne({ _id }, (error, data) => {
-        if (error) {
-          console.log(error);
-          reject(error);
-        }
-        resolve(data);
-      });
-    } catch (error) {
-      reject(error);
-    }
-  });
-};
-
-const storeUserRefreshJWT = (_id, token) => {
-  return new Promise((resolve, reject) => {
-    try {
-      UserSchema.findOneAndUpdate(
-        { _id },
-        {
-          $set: { "refreshJWT.token": token, "refreshJWT.addedAt": Date.now() },
-        },
-        { new: true }
-      )
-        .then((data) => resolve(data))
-        .catch((error) => {
-          console.log(error);
-          reject(error);
-        });
-    } catch (error) {
       console.log(error);
-      reject(error);
     }
   });
 };
 
-const updatePassword = (email, newhashedPass) => {
-  return new Promise((resolve, reject) => {
-    try {
-      UserSchema.findOneAndUpdate(
-        { email },
-        {
-          $set: { password: newhashedPass },
-        },
-        { new: true }
-      )
-        .then((data) => resolve(data))
-        .catch((error) => {
-          console.log(error);
-          reject(error);
-        });
-    } catch (error) {
-      console.log(error);
-      reject(error);
-    }
-  });
-};
-
-const verifyUser = (_id, email) => {
-  return new Promise((resolve, reject) => {
-    try {
-      UserSchema.findOneAndUpdate(
-        { _id, email, isVerified: false },
-        {
-          $set: { isVerified: true },
-        },
-        { new: true }
-      )
-        .then((data) => resolve(data))
-        .catch((error) => {
-          console.log(error.message);
-          reject(error);
-        });
-    } catch (error) {
-      console.log(error.message);
-      reject(error);
-    }
-  });
+const deletePin = (email, pin) => {
+  try {
+    ResetPinSchema.findOneAndDelete({ email, pin }, (error, data) => {
+      if (error) {
+        console.log(error);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = {
-  insertUser,
-  getUserByEmail,
-  getUserById,
-  storeUserRefreshJWT,
-  updatePassword,
-  verifyUser,
+  setPasswordRestPin,
+  getPinByEmailPin,
+  deletePin,
 };
