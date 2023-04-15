66a56e2a904e63f282bff0ba49e8fac16b0d6e05
prernasyan <prernasyan22@gmail.com>
2023-04-15 23:31:15 +0530

    ticket update and delete functions added

--- src/model/ticket/Ticket.model.js
+++ src/model/ticket/Ticket.model.js
@@ -34,3 +34,62 @@ const getTickets = (clientId) => {
       }
     });
   };
+  const updateClientReply = ({ _id, message, sender }) => {
+    return new Promise((resolve, reject) => {
+      try {
+        TicketSchema.findOneAndUpdate(
+          { _id },
+          {
+            status: "Pending operator response",
+            $push: {
+              conversations: { message, sender },
+            },
+          },
+          { new: true }
+        )
+          .then((data) => resolve(data))
+          .catch((error) => reject(error));
+      } catch (error) {
+        reject(error);
+      }
+    });
+  };
+  
+  const updateStatusClose = ({ _id, clientId }) => {
+    return new Promise((resolve, reject) => {
+      try {
+        TicketSchema.findOneAndUpdate(
+          { _id, clientId },
+          {
+            status: "Closed",
+          },
+          { new: true }
+        )
+          .then((data) => resolve(data))
+          .catch((error) => reject(error));
+      } catch (error) {
+        reject(error);
+      }
+    });
+  };
+  
+  const deleteTicket = ({ _id, clientId }) => {
+    return new Promise((resolve, reject) => {
+      try {
+        TicketSchema.findOneAndDelete({ _id, clientId })
+          .then((data) => resolve(data))
+          .catch((error) => reject(error));
+      } catch (error) {
+        reject(error);
+      }
+    });
+  };
+  
+  module.exports = {
+    insertTicket,
+    getTickets,
+    getTicketById,
+    updateClientReply,
+    updateStatusClose,
+    deleteTicket,
+  };
